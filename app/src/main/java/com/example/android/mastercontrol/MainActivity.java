ac24f63eaece6601016416167f2fe453bcbb4f68
ENerd03 <josepdg1@uci.edu>
2018-02-22 12:07:34 -0800

    Update

---
+++ app/src/main/java/com/example/android/mastercontrol/MainActivity.java
@@ -0,0 +1,232 @@
+package com.example.android.mastercontrol;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+import static android.R.attr.name;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    //grid variables
+    public boolean gridMode = true;
+    public boolean autoMode = false;
+
+    //variables for logging
+    float[] mGrav;
+    float[] mAcc;
+    float[] mGyro;
+    float[] mGeo;
+    File rrFile;
+    File jpgFile;
+    File recordingFile;
+    FileOutputStream fosRR;
+    Boolean logging;
+    Location dest_loc;
+    PixelGridView pixelGrid;
+    double[][] map;
+    double [][] orig_map;
+    ArrayList<Location> waypoints;
+    Location[][] gridLocations;
+    int currRow;
+    int currCol;
+
+    //Master variables
+    String fromMinion = "", //strings received from WiFi router
+            toMinion = "";  //strings to send through WiFi router
+    enum Robots {
+        DOC, MR, MRS, CARLITO, CARLOS, CARLY, CARLA, CARLETON,
+    }
+    Robots minion = Robots.DOC; //initial minion
+    double[] gps_coords;
+    double[] obstacle_coords = {0,0}; //initial position
+    int notFound = -1;
+    double[] mannequin_coords = {notFound,notFound};
+    int minion_grid_num_1, minion_grid_num_2, minion_grid_num_3; //add more for more robots
+    boolean isGridFullySearched = false;
+    boolean foundOBS, foundMANN; //implement later
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setup pixel grid
+        pixelGrid = new PixelGridView(this);
+
+        pixelGrid.setMapColors(orig_map, minion, (int) gps_coords[0], (int) gps_coords[1]);
+        pixelGrid.setId(View.generateViewId());
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(700, 700);
+        pixelGrid.setLayoutParams(lp);
+
+        ((LinearLayout) findViewById(R.id.leftll)).addView(pixelGrid);
+
+        //add functionality to gridMode button
+        Button buttonGrid = (Button) findViewById(R.id.btnGrid);
+        buttonGrid.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (!gridMode) {
+                    v.setBackgroundResource(R.drawable.button_grid_on);
+                    gridMode = true;
+                    pixelGrid.setVisibility(View.VISIBLE);
+                } else {
+                    v.setBackgroundResource(R.drawable.button_grid_off);
+                    gridMode = false;
+                    pixelGrid.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    //Called whenever the value of a sensor changes
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_GRAVITY)
+            mGrav = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE)
+            mGyro = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mAcc = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeo = event.values;
+
+
+        receive_from_m();
+        updateMap();
+
+        if (isGridFullySearched) {
+            //give new grid number
+
+        }
+
+        send_to_m(minion_grid_num_1);
+    }
+
+/**************************************************************************************************/
+    //receives info in form of string & parses to variables' appropriate types
+    void receive_from_m () {
+        String string_name = fromMinion.substring(fromMinion.indexOf("NAME"),fromMinion.indexOf("GPS")),
+                string_gps = fromMinion.substring(fromMinion.indexOf("GPS"),fromMinion.indexOf("OBS")),
+                string_obs = fromMinion.substring(fromMinion.indexOf("OBS"),fromMinion.indexOf("MANN")),
+                string_mann = fromMinion.substring(fromMinion.indexOf("MANN"),fromMinion.indexOf("SEARCH")),
+                string_search = fromMinion.substring(fromMinion.indexOf("SEARCH"),fromMinion.length());
+
+        //get name of minion that sent string
+        string_name = string_name.substring(string_name.indexOf(":")+1,string_name.indexOf(",")-1);
+
+        switch (string_name) {
+            case "DOC":
+                minion = Robots.DOC;
+                break;
+            case "MR":
+                minion = Robots.MR;
+                break;
+            case "MRS":
+                minion = Robots.MRS;
+                break;
+            case "CARLITO":
+                minion = Robots.CARLITO;
+                break;
+            case "CARLOS":
+                minion = Robots.CARLOS;
+                break;
+            case "CARLY":
+                minion = Robots.CARLY;
+                break;
+            case "CARLA":
+                minion = Robots.CARLA;
+                break;
+            case "CARLETON":
+                minion = Robots.CARLETON;
+                break;
+            default:
+                Log.i("ERROR","Invalid robot name. Refer to Robot name list in code.");
+        }
+
+        //get GPS coordinates from message string
+        gps_coords = getCoords(string_gps);
+
+        //get obstacle coordinates from message string
+        if (!string_obs.contains("none")) {
+            obstacle_coords = getCoords(string_obs);
+        }
+
+        //get mannequin coordinates from message string
+        if (!string_mann.contains("none")) {
+            mannequin_coords = getCoords(string_mann);
+        }
+
+        //get search boolean
+        if (string_search.contains("true")) {
+            isGridFullySearched = true;
+        } else {
+            isGridFullySearched = false;
+        }
+    }
+
+    void updateMap () {
+        if (mannequin_coords[0] == notFound) { //no mannequin found (coordinate will not = -1)
+            pixelGrid.setMapColors(orig_map, minion, (int) gps_coords[0], (int) gps_coords[1]);
+        } else { //mannequin found
+            pixelGrid.setMapColors(orig_map, minion,
+                    (int) gps_coords[0], (int) gps_coords[1],
+                    (int) mannequin_coords[0], (int) mannequin_coords[1]);
+
+            //reset condition test
+            mannequin_coords[0] = notFound;
+            mannequin_coords[1] = notFound;
+        }
+    }
+
+    //send grid # for m to search
+    void send_to_m (int current_grid_number) {
+        toMinion = "" + current_grid_number;
+
+        //actually send info here
+    }
+
+    double[] getCoords (String str) {
+        int find_comma, find_colon;
+
+        //find lat
+        find_colon = str.indexOf(':');
+        find_comma = str.indexOf(',');
+        String first_num = str.substring(find_colon+1, find_comma-1);
+
+        //cut out lat
+        str = str.substring(find_comma + 1, str.length());
+
+        //find lon
+        find_colon = str.indexOf(':');
+        find_comma = str.indexOf(']');
+        String sec_num = str.substring(find_colon+1, find_comma-1);
+
+
+        double[] coords={0,0};
+        double d = Double.parseDouble(first_num);
+        double d2 = Double.parseDouble(sec_num);
+        coords[0] = d;
+        coords[1] = d2;
+        return coords;
+    }
+
+}
---
+++ app/src/main/java/com/example/android/mastercontrol/PixelGridView.java
@@ -0,0 +1,198 @@
+package com.example.android.mastercontrol;
+
+        import android.content.Context;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Paint;
+        import android.util.AttributeSet;
+        import android.util.Log;
+        import android.view.View;
+
+        import static android.R.attr.name;
+
+/**
+ * Created by tiffany on 7/10/16.
+ */
+public class PixelGridView extends View {
+    private int numColumns, numRows;
+    private int cellWidth, cellHeight;
+    private Paint paint = new Paint();
+    private boolean[][] cellChecked;
+    private int[][] gridColors = new int[0][0];
+
+    public PixelGridView(Context context) {
+        this(context, null);
+    }
+
+    public PixelGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    public int getNumColumns() {
+        return numColumns;
+    }
+
+    public void setNumColumns(int numColumns) {
+        this.numColumns = numColumns;
+        calculateDimensions();
+    }
+
+    public int getNumRows() {
+        return numRows;
+    }
+
+    public void setNumRows(int numRows) {
+        this.numRows = numRows;
+        calculateDimensions();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        calculateDimensions();
+    }
+
+    private void calculateDimensions() {
+        if (numColumns < 1 || numRows < 1) {
+            return;
+        }
+
+        cellWidth = getWidth() / numColumns;
+        cellHeight = getHeight() / numRows;
+
+        cellChecked = new boolean[numColumns][numRows];
+
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+
+        if (numColumns == 0 || numRows == 0) {
+            return;
+        }
+
+        int width = getWidth();
+        int height = getHeight();
+
+        for (int i = 0; i < numColumns; i++) {
+            for (int j = 0; j < numRows; j++) {
+                float[] col = {(float)gridColors[j][i],(float)1,(float)1};
+                paint.setColor(Color.HSVToColor(col));
+                canvas.drawRect(i * cellWidth, j * cellHeight,
+                        (i + 1) * cellWidth, (j + 1) * cellHeight,
+                        paint);
+            }
+        }
+
+        for (int i = 1; i < numColumns; i++) {
+            canvas.drawLine(i * cellWidth, 0, i * cellWidth, height, paint);
+        }
+
+        for (int i = 1; i < numRows; i++) {
+            canvas.drawLine(0, i * cellHeight, width, i * cellHeight, paint);
+        }
+    }
+
+    public void setGridColors(int[][] cost, int route[][], int currR, int currC){
+        numRows = cost.length;
+        numColumns = cost[0].length;
+        gridColors = new int[cost.length][cost[0].length];
+        int maxCost = Integer.MIN_VALUE;
+        int minCost = Integer.MAX_VALUE;
+        for(int i = 0; i < cost.length; i++){
+            for(int j = 0; j < cost[0].length; j++){
+                if(cost[i][j] > maxCost)
+                    maxCost = cost[i][j];
+                if(cost[i][j] < minCost)
+                    minCost = cost[i][j];
+            }
+        }
+        for(int i = 0; i < cost.length; i++){
+            for(int j = 0; j < cost[0].length; j++){
+                int hue = (int)(((float)(cost[i][j] - minCost))/(maxCost-minCost)*180)+180;
+                gridColors[i][j] = hue;
+            }
+        }
+        for(int i = 0; i < route.length; i++){
+            gridColors[route[i][0]][route[i][1]] = 60;//yellow
+        }
+        gridColors[currR][currC] = 120;//green
+        invalidate();
+    }
+
+    //call when no mannequin is found
+    public void setMapColors(double[][] map, MainActivity.Robots name, int currR, int currC){
+        numRows = map.length;
+        numColumns = map[0].length;
+        gridColors = new int[numRows][numColumns];
+
+        if (name == MainActivity.Robots.DOC) {
+            gridColors[currR][currC] = 80;
+        }
+        else if (name == MainActivity.Robots.MR) {
+            gridColors[currR][currC] = 85;
+        }
+        else if (name == MainActivity.Robots.MRS) {
+            gridColors[currR][currC] = 90;
+        }
+        else if (name == MainActivity.Robots.CARLITO) {
+            gridColors[currR][currC] = 95;
+        }
+        else if (name == MainActivity.Robots.CARLOS) {
+            gridColors[currR][currC] = 100;
+        }
+        else if (name == MainActivity.Robots.CARLY) {
+            gridColors[currR][currC] = 105;
+        }
+        else if (name == MainActivity.Robots.CARLA) {
+            gridColors[currR][currC] = 110;
+        }
+        else if (name == MainActivity.Robots.CARLETON) {
+            gridColors[currR][currC] = 115;
+        } else {
+            Log.i("PixelGridView Error","Robot name not defined");
+        }
+        invalidate();
+
+    }
+
+    //call when mannequin is found
+    public void setMapColors(double[][] map, MainActivity.Robots name, int currR, int currC, int mannR, int mannC){
+        numRows = map.length;
+        numColumns = map[0].length;
+        gridColors = new int[numRows][numColumns];
+
+
+        if (name == MainActivity.Robots.DOC) {
+            gridColors[currR][currC] = 80;
+        }
+        else if (name == MainActivity.Robots.MR) {
+            gridColors[currR][currC] = 85;
+        }
+        else if (name == MainActivity.Robots.MRS) {
+            gridColors[currR][currC] = 90;
+        }
+        else if (name == MainActivity.Robots.CARLITO) {
+            gridColors[currR][currC] = 95;
+        }
+        else if (name == MainActivity.Robots.CARLOS) {
+            gridColors[currR][currC] = 100;
+        }
+        else if (name == MainActivity.Robots.CARLY) {
+            gridColors[currR][currC] = 105;
+        }
+        else if (name == MainActivity.Robots.CARLA) {
+            gridColors[currR][currC] = 110;
+        }
+        else if (name == MainActivity.Robots.CARLETON) {
+            gridColors[currR][currC] = 115;
+        } else {
+            Log.i("PixelGridView Error","Robot name not defined");
+        }
+        gridColors[mannR][mannC] = 90;// <----check color!!!
+        invalidate();
+    }
+}
